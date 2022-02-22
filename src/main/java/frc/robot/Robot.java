493befa06cdfedcf40b34ee5d01316e2eaba95d1
swirl <roachh@protonmail.com>
2022-02-22 17:51:09 -0500

    the

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.Limelight;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -19,6 +20,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+  private Limelight _l;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -29,6 +31,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    _l = new Limelight();
   }
 
   /**
@@ -89,7 +92,10 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    _l.distance();
+    _l.putTargetValues();
+  }
 
   @Override
   public void testInit() {
--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -83,7 +83,7 @@ public class Limelight extends SubsystemBase {
         distEst = distEstTotal / distEstIters;
         distEstTotal = 0;
         distEstIters = 0;
-        put("Distance", distEst);
+        put("Distance", distEst / 12);
       }
 
       /*// EXCEL OP
@@ -96,10 +96,12 @@ public class Limelight extends SubsystemBase {
       // Very confident. but will probably need to use PID to
       // always face the same way.*/
       double targH = 84.;
-      double mountH = 3.5;
+      double mountH = 21.;//3.5;
       double mountA = 45;
       double dist = (targH - mountH) /
-                    Math.tan(Math.toRadians(mountA + getX()));
+                    Math.tan(Math.toRadians(mountA + getX()))/* *
+                    Math.cos(getY()))*/;
+      put("newdist", dist);
       distEstTotal += dist;
       distEstIters++;
     }
