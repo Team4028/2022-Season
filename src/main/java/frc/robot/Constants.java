4b9f2ccbc42d70762c7361c1071337eb7a73a967
gabeseq <gabe.seq@gmail.com>
2022-03-05 12:06:14 -0500

    DEVELOPMENT BRANCH IS NOW MK4 SWERVE

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,8 +16,6 @@ import edu.wpi.first.math.trajectory.TrapezoidProfile;
  * constants. This class should not be used for any other purpose. All constants
  * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>
  * It is advised to statically import this class (or one of its inner classes)
  * wherever the
  * constants are needed, to reduce verbosity.
