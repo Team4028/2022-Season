50016e9f9cd2bf2bd2689e3f51a2e2fb73c91312
swirl <swurl@swurl.xyz>
2022-03-20 13:44:35 -0400

    fix infeed

--- src/main/java/frc/robot/subsystems/Infeed.java
+++ src/main/java/frc/robot/subsystems/Infeed.java
@@ -35,7 +35,7 @@ public class Infeed extends SubsystemBase {
   }
 
   public void runInfeedSingulatorMotors(double mult) {
-    if (!_solenoid.get()) {
+    if (_solenoid.get()) {
       _infeedMotor.set(ControlMode.PercentOutput, mult * VBusConstants.kInfeed);
       SmartDashboard.putBoolean("Infeed/Running", true);
       SmartDashboard.putNumber("Infeed/Vbus", mult * VBusConstants.kInfeed);
