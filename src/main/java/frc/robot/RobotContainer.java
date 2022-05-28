1df21ef85a2afbf1e66307ce4397c088fc3de7b7
swirl <swurl@swurl.xyz>
2022-05-28 15:52:35 -0400

    switch around some buttons

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -154,10 +154,10 @@ public class RobotContainer {
         // ====================================
 
         // ======== DRIVER CONTROLLER ========
-        m_driverController.a.whenPressed(new ToggleLEDMode());
+        m_driverController.a.whenPressed(new InstantCommand(() -> toggleFieldOriented()));
         m_driverController.b.whenPressed(new ToggleTippy());
         m_driverController.x.whenPressed(new MagicShootMovingCommand());
-        m_driverController.y.whenPressed(new InstantCommand(() -> toggleFieldOriented()));
+        m_driverController.y.whenPressed(new ToggleLEDMode());
         m_driverController.start.whenPressed(new InstantCommand(() -> m_drive.zeroHeading()));
         m_driverController.lb.toggleWhenPressed(new ReverseInfeedAndConveyor());
         m_driverController.rb.whenPressed(new ToggleInfeedUp());
